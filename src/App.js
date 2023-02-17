2c26d9745ee34a948f56c00cf29cbff60b79807b
Soumyadeep448 <sbhattacharya448@gmail.com>
2023-02-17 19:14:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import Alert from "./Components/Alert";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
 function App() {
+   
+  const darkMode='Dark Mode';
+  const lightMode='Light Mode';
+
+  const [mode,setMode]=useState('light');
+  const [btnText,setbtnText]=useState('Dark Mode');
+  const [formText,setformText]=useState('dark');
+  const [alert,setalert]=useState(null);
+  
+
+  let showAlert=(modename)=>{
+    setalert('Success: '+modename);
+    setTimeout(() => {
+      setalert(null);
+    }, 1500);
+  }
+
+  let toggleMode=()=>{
+    if(mode==='dark')
+    {
+      setMode('light');
+      setbtnText('Dark Mode');
+      setformText('dark');
+      showAlert(lightMode);
+      document.body.style.backgroundColor='white';
+      
+    }
+    else{
+      setMode('dark');
+      setbtnText('Light Mode');
+      setformText('light');
+      showAlert(darkMode);
+      document.body.style.backgroundColor='#464646';
+      
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar title="TextApp" mode={mode} toggleMode={toggleMode} btnText={btnText}/>
+    <Alert alert={alert}  />
+    <TextForm here="Enter Text:" formText={formText} showAlert={showAlert}/>
+    </>
   );
 }
 
